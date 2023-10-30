74805b13683e5dcff6ecc43556ed14a3ea6c3b50
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-10-30 17:15:45 +0600

    EmptyInput Handled

--- ToDo.js
+++ ToDo.js
@@ -1,24 +1,43 @@
-const addToDo=()=>{
-    let title = document.getElementById('todo-title').value;
-    let desc = document.getElementById('todo-desc').value;
-    localStorage.setItem(title,desc);
-    displayToDO();
-}
-const removeToDo=(key,elem)=>{
-    localStorage.removeItem(key);
-    toDoElem = elem.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;;
-    toDoElem.remove()
-}
-const MarkDone = (elem) =>{
-    elem.classList.toggle('bg-success');
-}
+const addToDo = () => {
+  let title = document.getElementById("todo-title").value;
+  let desc = document.getElementById("todo-desc").value;
+  let notify = document.getElementById("notification");
+  let notifyContent = "";
+  if (!title || !desc) {
+    // Close the modal 
+ 
+
+
+    notifyContent += `
+        <div class="alert alert-warning alert-dismissible fade show" role="alert">
+            <strong>Error!</strong> Both field are required.
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+        `;
+       
+  }
+  notify.innerHTML = notifyContent;
+  localStorage.setItem(title, desc);
+  displayToDO();
+};
+const removeToDo = (key, elem) => {
+  localStorage.removeItem(key);
+  toDoElem =
+    elem.parentElement.parentElement.parentElement.parentElement.parentElement
+      .parentElement.parentElement;
+  toDoElem.remove();
+};
+const MarkDone = (elem) => {
+  elem.classList.toggle("bg-success");
+};
+
 
 let displayContainer = document.getElementById("displayContainer");
-const displayToDO = () =>{
-    let displayContent = ""
-    for (let key in localStorage) {
-        if (localStorage.hasOwnProperty(key)) {
-            displayContent += `<div class="row m-4">
+const displayToDO = () => {
+  let displayContent = "";
+  for (let key in localStorage) {
+    if (localStorage.hasOwnProperty(key)) {
+      displayContent += `<div class="row m-4">
             <div class="card">
               <div class="card-body row">
                 <div class="col-sm-9 mb-3 mb-sm-0">
@@ -35,10 +54,14 @@ const displayToDO = () =>{
                 </div>
               </div>
             </div>
-          </div>`
-        }
-        
+          </div>`;
     }
-    displayContainer.innerHTML = displayContent;
-}
+  }
+  displayContainer.innerHTML = displayContent;
+};
 displayToDO();
+
+
+// Close the modal by ID
+
+  
