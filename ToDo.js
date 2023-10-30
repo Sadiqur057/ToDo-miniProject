ec8ce2f69b9fdcbb269a8a83f354f3d97371eb17
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2023-10-30 16:10:49 +0600

    removeTodo() added

--- ToDo.js
+++ ToDo.js
@@ -2,12 +2,17 @@ const addToDo=()=>{
     let title = document.getElementById('todo-title').value;
     let desc = document.getElementById('todo-desc').value;
     localStorage.setItem(title,desc);
+    displayToDO();
+}
+const removeToDo=(key,elem)=>{
+    localStorage.removeItem(key);
+    toDoElem = elem.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;;
+    toDoElem.remove()
 }
 
-
 let displayContainer = document.getElementById("displayContainer");
-let displayContent = ""
 const displayToDO = () =>{
+    let displayContent = ""
     for (let key in localStorage) {
         if (localStorage.hasOwnProperty(key)) {
             displayContent += `<div class="row m-4">
@@ -21,7 +26,7 @@ const displayToDO = () =>{
                   <table>
                     <tr>
                       <td><a href="#" class="btn btn-primary mb-1">Complete</a></td>
-                      <td><a href="#" class="btn btn-danger mb-1">Remove</a></td>
+                      <td><button type="button" class="btn btn-danger" onclick="removeToDo('${key}',this)">Remove</button></td>
                     </tr>
                   </table>
                 </div>
